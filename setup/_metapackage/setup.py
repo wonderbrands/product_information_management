c4c0cc44166526db4c715e5a222fdf00b2a01a4c
William Colin <williamcolin954@gmail.com>
2022-06-02 17:14:22 -0500

    [UPD] Github Oca Version
    Se actualiza la version al Github de la OCA

---
+++ setup/_metapackage/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo12-addons-oca-odoo-pim",
+    description="Meta package for oca-odoo-pim Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo12-addon-attribute_set',
+        'odoo12-addon-attribute_set_completeness',
+        'odoo12-addon-attribute_set_mass_edit',
+        'odoo12-addon-attribute_set_searchable',
+        'odoo12-addon-pim',
+        'odoo12-addon-product_attribute_set',
+        'odoo12-addon-product_attribute_set_completeness',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 12.0',
+    ]
+)
